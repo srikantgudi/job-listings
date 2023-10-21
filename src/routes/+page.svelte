0a2598614597337f357ffcf9f14a717b508df0dc
Srikant Gudi <srikantgudi@gmail.com>
2023-10-21 16:43:06 +0530

    modified layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -52,17 +52,7 @@
 <style>
   main {
     width: 1440px;
-    margin: auto;
-  }
-  .company {
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-    border-radius: 50%;
-    box-shadow: 0 0 4px #666;
-    width: 6rem;
-    height: 6rem;
-    padding: 1rem;
+    margin: 0 auto;
   }
   .job {
     box-sizing: border-box;
@@ -74,16 +64,6 @@
     padding: 1rem 1rem 0 2rem;
     width: 100%;
   }
-  .job.active {
-    border-left: 6px solid teal;
-    border-radius: 1rem auto auto 1rem;
-  }
-  .tools {
-    display: flex;
-    align-items: center;
-    gap: 1rem;
-    justify-content: flex-end;
-  }
   @media screen and (max-width:375px) {
     .job {
       grid-template-columns: 1fr;
@@ -92,60 +72,66 @@
   }
 </style>
 <main>
-  <div class="row p-2 mx-2 border rounded-5">
-    <div class="col">
-      {#if roleFilter}
-        {roleFilter} <button on:click={() => roleFilter = ''} class="btn btn-sm rounded">&times;</button>
-      {/if}
-      {#if levelFilter}
-        {levelFilter} <button on:click={() => levelFilter = ''} class="btn btn-sm rounded">&times;</button>
-      {/if}
-      {#if langs.length}
-        {#each langs as l}
-        {l} <button on:click={() => clearLang(l)} class="btn btn-sm rounded">&times;</button>
-        {/each}
-      {/if}
-      {#if tools.length}
-        {#each tools as t}
-        {t} <button on:click={() => clearTool(t)} class="btn btn-sm rounded">&times;</button>
-        {/each}
-      {/if}
+  <div>
+    <div class="invisible md:visible">
+      <img class="sticky mt-[-1rem] bg-gray-400" src="/images/bg-header-desktop.svg" alt="bg" />
     </div>
-    <div class="col-1">
-      <button class="btn btn-sm rounded-pill" on:click={clearFilters}>Clear</button>
+    <div class="visible md:hidden">
+      <img class="sticky bg-gray-400 w-full" src="/images/bg-header-mobile.svg" alt="bg" />
+    </div>
+    <div class="absolute shadow-md rounded-md px-4 mt-[-1rem] text-2 h-[3rem] md:w-[70%] w-full border mx-12 bg-white mt-6 p-1 flex items-center justify-between">
+      <!-- <div class="flex gap-0"> -->
+        {#if roleFilter}
+          <div class="mr-1 px-2 flex gap-0"><span class="bg-gray-300 px-2 rounded-l-md">{roleFilter}</span><button on:click={() => roleFilter = ''} class="bg-teal-400 hover:bg-teal-600 px-2 text-white rounded-r-md">&times;</button></div>
+        {/if}
+        {#if levelFilter}
+        <div class="mr-1 px-2 flex gap-0"><span class="bg-gray-300 px-2 rounded-l-md">{levelFilter}</span> <button on:click={() => levelFilter = ''} class="bg-teal-400 hover:bg-teal-600 px-2 text-white rounded-r-md">&times;</button></div>
+        {/if}
+        {#if langs.length}
+          {#each langs as l}
+          <div class="mr-2 px-2 flex gap-0"><span class="bg-gray-300 px-2 rounded-l-md">{l}</span> <button on:click={() => clearLang(l)} class="bg-teal-400 hover:bg-teal-600 px-2 text-white rounded-r-md">&times;</button></div>
+          {/each}
+        {/if}
+        {#if tools.length}
+          {#each tools as t}
+          <div class="mr-2">{t} ?<button on:click={() => clearTool(t)} class="btn btn-sm rounded">&times;</button></div>
+          {/each}
+        {/if}
+      <!-- </div>
+      <div class="ml-auto"> -->
+        <button class="ml-auto" on:click={clearFilters}>Clear</button>
+      <!-- </div> -->
     </div>
   </div>
-  <div>
+  <div class="my-12">
     {#each data.jobs.filter(f => filterRow(f)) as row}
-      <div class="d-flex my-md-2">
-        <div class="job" class:active={row.featured}>
-          <div>
-            <img src={row.logo} alt="logo" />
+      <div class="flex gap-2 my-4 shadow-lg {row.featured ? 'border-l-4 border-teal-600': ''} p-4 rounded-l-lg">
+        <div class="mr-4">
+          <img src={row.logo} alt="logo" />
+        </div>
+        <div class="w-1/2">
+          <div class="flex gap-1 gap-md-2 align-items-center">
+            <span class="text-xl mr-4">{row.company}</span>
+            {#if row.new}<span class="bg-teal-400 px-2 text-white rounded-2xl">New!</span>{/if}
+            {#if row.featured}<span class="bg-teal-800 px-2 text-white rounded-2xl">Featured</span>{/if}
           </div>
-          <div class="d-flex flex-column gap-2">
-            <div class="d-flex gap-1 gap-md-2 align-items-center">
-              <span class="fs-5">{row.company}</span>
-              {#if row.new}<span class="badge badge-primary rounded-pill">New!</span>{/if}
-              {#if row.featured}<span class="badge badge-primary rounded-pill">Featured</span>{/if}
-            </div>
-            <div class="fs-5 fw-bold">
-              {row.position}
-            </div>
-            <div class="d-flex gap-4 align-items-center">
-              <span>{row.postedAt}</span> &bull; <span>{row.contract}</span> &bull; <span>{row.location}</span>
-            </div>
+          <div class="fs-5 fw-bold">
+            {row.position}
           </div>
-          <!-- tools, etc-->
-          <div class="d-flex justify-content-end fs-5 gap-2 py-5">
-            <button class="btn badge badge-primary rounded-pill" on:click={setRoleFilter(row.role)}>{row.role}</button>
-            <button class="btn badge badge-primary rounded-pill" on:click={setLevelFilter(row.level)}>{row.level}</button>
-            {#each row.languages as lang}
-            <button class="btn badge badge-primary rounded-pill" on:click={() => addLang(lang)}>{lang}</button>
-            {/each}
-            {#each row.tools as tool}
-            <span class="btn badge badge-primary rounded-pill" on:click={() => addTool(tool)}>{tool}</span>
-            {/each}
+          <div class="d-flex gap-4 align-items-center">
+            <span>{row.postedAt}</span> &bull; <span>{row.contract}</span> &bull; <span>{row.location}</span>
           </div>
+        </div>
+        <!-- tools, etc-->
+        <div class="flex gap-2 my-8">
+          <button class="bg-slate-200 rounded-md px-2 text-teal-500 font-bold" on:click={setRoleFilter(row.role)}>{row.role}</button>
+          <button class="bg-slate-200 rounded-md px-2 text-teal-500 font-bold" on:click={setLevelFilter(row.level)}>{row.level}</button>
+          {#each row.languages as lang}
+          <button class="bg-slate-200 rounded-md px-2 text-teal-500 font-bold" on:click={() => addLang(lang)}>{lang}</button>
+          {/each}
+          {#each row.tools as tool}
+          <button class="bg-slate-200 rounded-md px-2 text-teal-500 font-bold" on:click={() => addTool(tool)}>{tool}</button>
+          {/each}
         </div>
       </div>
     {/each}
